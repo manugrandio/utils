868e72d60fa3e252851529dca0903ff45eea6ed0
Manu Grandío <manugrandio@fastmail.fm>
2016-12-03 12:25:14 +0100

    Install matchit plugin in vim

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -7,5 +7,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo Installing snipMate...
 git clone https://github.com/msanders/snipmate.vim ~/.vim/bundle/snipmate
 
-echo Installing NERD Commenter
+echo Installing NERD Commenter...
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+
+echo Installing matchit...
+git clone https://github.com/vim-scripts/matchit.zip ~/.vim/bundle/matchit
