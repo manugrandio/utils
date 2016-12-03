18d2d6aaa53faa3c5fc90276ea8937e715766335
Manu Grandío <manugrandio@fastmail.fm>
2016-12-03 12:14:51 +0100

    Add script that installs pathogen and vim plugins.

---
+++ install-vim-plugins.sh
@@ -0,0 +1,11 @@
+# Install vim plugins
+
+echo Installing Pathogen...
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo Installing snipMate...
+git clone https://github.com/msanders/snipmate.vim ~/.vim/bundle/snipmate
+
+echo Installing NERD Commenter
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
