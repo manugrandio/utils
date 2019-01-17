94653c5969d0347d884707c618c8cd8d1a03c0f2
Manu Grandío <manugrandio@fastmail.fm>
2019-01-17 10:19:43 +0100

    New layout: main window for vim + 3 panes for rest

--- tmux-basic-session.sh
+++ tmux-basic-session.sh
@@ -2,11 +2,12 @@
 tmux new-session -s dev -n vim -d
 
 # Create a big pane on the left and two small ones on the right
-tmux split-window -h -t dev
-tmux split-window -v -t dev:0.1
+tmux new-window
+tmux split-window -h -t dev:1
+tmux split-window -v -t dev:1.1
+tmux select-pane -t 1.0
 
-# Select the left pane
-tmux select-pane -L
+tmux select-window -t 0
 
 # Attach to the session
 tmux attach -t dev
